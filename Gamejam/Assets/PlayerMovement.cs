e0d9d7f2d7462dff63ed99e97addf2f307a5d130
slimerplanet <eppuku@gmail.com>
2021-01-23 14:23:30 +0200

    player

--- Gamejam/Assets/PlayerMovement.cs
+++ Gamejam/Assets/PlayerMovement.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Movespeed = 5;
+    public Rigidbody2D rb;
+    public Camera cam;
+    public Transform rotationPoint;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    private void Update()
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+
+       mousePos = cam.ScreenToWorldPoint( Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Movespeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos  - (Vector2)rotationPoint.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90;
+
+        rotationPoint.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
 }
