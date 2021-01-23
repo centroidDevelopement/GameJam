6ab958513e597e1d3828283cbe3cc4cb9ca43d46
slimerplanet <eppuku@gmail.com>
2021-01-23 14:52:43 +0200

    a* pathfinding

---
+++ Gamejam/Assets/shooting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    private void shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        var rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+    }
+}
