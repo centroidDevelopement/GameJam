58040d89bf0889c8dbde64af35009a0d8dc2d06d
slimerplanet <eppuku@gmail.com>
2021-01-23 15:14:05 +0200

    health enemy

---
+++ Gamejam/Assets/playerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    public int damageTaken;
+    public int health;
+    public int maxHealth = 5;
+
+    private void Start()
+    {
+        maxHealth = 5;
+    }
+
+    private void Update()
+    {
+        health = maxHealth - damageTaken;
+    }
+
+}
