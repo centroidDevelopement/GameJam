b94b079bbd0dc4031fab70a5d32b9f575c8ad26c
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2021-01-24 12:51:49 +0200

    ignore these

--- Gamejam/Assets/gridManager.cs
+++ Gamejam/Assets/gridManager.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class gridManager : MonoBehaviour
 {
+    Grid grid;
+    int value;
     private void Start()
     {
-        Grid grid = new Grid(20, 10);
+        grid = new Grid(4, 2, 10f, new Vector3(0, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 10);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
     }
 }
