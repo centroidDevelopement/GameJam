3a2dd3a73a2db6ed4d59b7ff06074b6797f1dc39
Swatpvp <67360231+Swatpvp@users.noreply.github.com>
2021-01-24 11:09:02 +0200

    Griding

---
+++ Gamejam/Assets/gridManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(20, 10);
+    }
+}
